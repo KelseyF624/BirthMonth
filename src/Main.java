4591ded9f21ae76f03b615318ab8735ea1b64e4d
KelseyF624 <kelseyf.624@gmail.com>
2025-02-04 14:51:57 -0500

    Complete Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Please enter the month you were born.(1-12)");
+        Scanner in = new Scanner(System.in);
+        int month = in.nextInt();
+        if (month < 1 || month > 12) { System.out.println("Invalid month. You entered "+ month); return; }
+        else if (month >= 1 || month <= 12) {System.out.println("Your birth month is " + month);}
     }
 }
